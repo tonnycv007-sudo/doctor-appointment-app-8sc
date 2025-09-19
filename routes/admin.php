a8ca43215ec5e191adfbd53d63131ed7cc85cede
Tony Ventura <tonnycv007@gmail.com>
2025-09-18 18:50:12 -0600

    feat(admin):starting to add FlowBite

---
+++ routes/admin.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/',function(){
+  return view('admin.dashboard');
+})->name('dashboard');
+
+
+?>
+
+
+
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::middleware([
     'auth:sanctum',
